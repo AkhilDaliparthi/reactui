2a880c68ae90005a0fc863a7850cd789f5e0d1b5
Akhil Daliparthi <akhilaki654@gmail.com>
2019-10-06 16:00:51 +0530

    React Synchronus call changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { loginAPI } from "./callApi";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      response: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.response = this.response.bind(this);
+    this.login = this.login.bind(this);
+  }
+
+  login(response) {
+    loginAPI("login/", response);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  response() {
+    var response = {};
+    response["username"] = this.state.username;
+    response["password"] = this.state.password;
+    response["platform"] = "website";
+    this.login(response);
+  }
+
+  render() {
+    return (
+      <Form className="login">
+        <FormGroup>
+          <Label> Username </Label>
+          <Input
+            type="text"
+            placeholder="username"
+            name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label> Password </Label>
+          <Input
+            type="password"
+            placeholder="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <Button className="btn-lg btn-dark btn-block" onClick={this.response}>
+          Login
+        </Button>
+      </Form>
+    );
+  }
 }
 
 export default App;
